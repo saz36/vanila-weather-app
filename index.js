6c51ff132cc6644a249f26b1c34fe99cb66bbeb9
saz36 <124158555+saz36@users.noreply.github.com>
2023-02-16 10:29:05 +0000

    updated date changed layout

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 function showUser(response) {
   let descriptionElement = document.querySelector("#description");
   let cityElement = document.querySelector("#results");
   let tempElement = document.querySelector("#temp-icon");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
-
+  let dateElement = document.querySelector("#date");
   console.log(response.data);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   tempElement.innerHTML = Math.round(response.data.main.temp);
   windElement.innerHTML = Math.round(response.data.wind.speed);
   humidityElement.innerHTML = Math.round(response.data.main.humidity);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 function search(city) {
